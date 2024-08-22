207980cffb44bdb535ca5607d6c383644ca027f1
jangwoolee <8282qwe@gmail.com>
2024-08-22 21:06:43 +0900

    "*"

--- src/main/java/com/icebuckwheat/oauthserver/Config/WebConfig.java
+++ src/main/java/com/icebuckwheat/oauthserver/Config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.icebuckwheat.oauthserver.Config;
 
-import jakarta.ws.rs.HttpMethod;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,13 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:8080","https://reacticeout.icebuckwheat.kro.kr")
-                .allowedMethods(
-                        HttpMethod.GET,
-                        HttpMethod.HEAD,
-                        HttpMethod.POST,
-                        HttpMethod.PUT,
-                        HttpMethod.DELETE
-                )
+                .allowedMethods("*")
                 .allowCredentials(true) // 이 부분이 중요합니다.
                 .allowedHeaders("*");
     }
