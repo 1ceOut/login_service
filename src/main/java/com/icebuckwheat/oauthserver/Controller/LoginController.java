33bf49fc65f388d7bc7f75db674a1daeb1d21c09
jangwoolee <8282qwe@gmail.com>
2024-08-23 10:42:39 +0900

    preflight5

--- src/main/java/com/icebuckwheat/oauthserver/Controller/LoginController.java
+++ src/main/java/com/icebuckwheat/oauthserver/Controller/LoginController.java
@@ -114,7 +114,7 @@ public class LoginController {
                 .body(jwtResponse);
     }
 
-    @GetMapping("/login/logout")
+    @DeleteMapping("/login/logout")
     public ResponseEntity<Object> logout(@CookieValue(value = "refreshToken",required = false)String refreshToken) throws JsonProcessingException {
         if (refreshToken==null || refreshToken.isEmpty()) return ResponseEntity.status(210).build();
 
