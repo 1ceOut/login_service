6e5e323f0892fb73d5a5e36a4a42903ac77b6add
jangwoolee <8282qwe@gmail.com>
2024-08-23 10:57:34 +0900

    preflight5

--- src/main/java/com/icebuckwheat/oauthserver/Config/WebConfig.java
+++ src/main/java/com/icebuckwheat/oauthserver/Config/WebConfig.java
@@ -1,18 +1,14 @@
 package com.icebuckwheat.oauthserver.Config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 모든 경로에 대해 CORS 설정 적용
-                .allowedOrigins("http://localhost:8080", "https://reacticeout.icebuckwheat.kro.kr")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")  // 모든 경로에 대해 CORS 설정 적용
+//                .allowedOrigins("http://localhost:8080", "https://reacticeout.icebuckwheat.kro.kr")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
